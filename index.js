38da737a26c5807aa4d95677d75de92ede376e98
Matthijs van Henten <matthijs@ischen.nl>
2014-08-12 00:48:38 +0200

    tidied linted

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
+'use strict';
+
 var Parser = require('less').Parser,
     async = require('async'),
     fs = require('fs'),
     Path = require('path'),
-    _   = require('lodash');
+    _ = require('lodash');
 
-function compile( item, done ){
-    item.target = item.target.replace( /[.]less$/, '.css' );
+function compile(item, done) {
+    item.target = item.target.replace(/[.]less$/, '.css');
 
-    fs.readFile( item.source, function( err, src ){
+    fs.readFile(item.source, function(err, src) {
         var parser = new Parser({
-            paths: [ Path.dirname( item.source ) ],
-            filename: Path.basename( item.source )
+            paths: [Path.dirname(item.source)],
+            filename: Path.basename(item.source)
         });
 
-        parser.parse( src.toString(), function( err, tree ){
-            if( err ) throw err;
+        parser.parse(src.toString(), function(err, tree) {
+            if (err) throw err;
             item.content = tree.toCSS();
+
             done();
         });
     });
 }
 
-module.exports = function( config, done ){
-    var lessSources = _(config.sources).where(function(source){
-        return source.type === 'file' && Path.extname( source.source ) === '.less';
+module.exports = function(config, done) {
+    var lessSources = _(config.sources).where(function(source) {
+        return source.type === 'file' && Path.extname(source.source) === '.less';
     }).valueOf();
 
-    async.each( lessSources, compile, done );
+    async.each(lessSources, compile, done);
 };
